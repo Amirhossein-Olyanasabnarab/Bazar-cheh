b663cbd34e7d45dda7111bc79dad7f52d8953bdf
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-09-09 03:56:54 +0330

    intalling and using auto-bind

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,5 +1,10 @@
+const autoBind = require('auto-bind');
+const authService = require('./auth.service');
 class AuthController {
-
+    #service;
+    constructor() {
+        this.#service = authService;
+        autoBind(this);
+    }
 }
-
-module.exports = new AuthController();
+module.exports = new AuthController(); 
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,5 +1,10 @@
-class AuthService {
+const autoBind = require('auto-bind');
 
+class AuthService {
+    
+    constructor(){
+        autoBind(this);
+    }
 }
 
 module.exports = new AuthService();
