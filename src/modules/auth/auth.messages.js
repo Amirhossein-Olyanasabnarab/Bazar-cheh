a728796aa596a1ec1d4ec047802ca9b0b6854506
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-09-10 21:46:31 +0330

    checkExistUserByMobile

--- src/modules/auth/auth.messages.js
+++ src/modules/auth/auth.messages.js
@@ -1,5 +1,6 @@
 const AuthMessages = {
     sendOtpSuccessfully: "OTP sent successfully",
+    NotFound: "User not found",
 };
 
 module.exports = AuthMessages;
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,13 +1,22 @@
 const autoBind = require('auto-bind');
-
+const UserModel = require('../user/user.model');
+const createHttpError = require('http-errors');
+const AuthMessages = require('./auth.messages');
 class AuthService {
-    
+    #model;
     constructor(){
         autoBind(this);
+        this.#model = UserModel
     }
 
-    async sendOtp() {
-        
+    async sendOtp(mobile) {
+         
+    }
+
+    async checkExistByMobile(mobile){
+        const user = await UserModel.findOne({mobile});
+        if(!user) throw new createHttpError(AuthMessages.NotFound);
+        return user;
     }
 }
 
