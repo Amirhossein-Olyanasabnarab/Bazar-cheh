df038263ba2a057b9f106f0467a826b345832f53
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-09-13 14:17:39 +0330

    AuthController & AuthService

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,26 +1,26 @@
-const autoBind = require('auto-bind');
-const authService = require('./auth.service');
-const AuthMessages = require('./auth.messages');
+const autoBind = require("auto-bind");
+const authService = require("./auth.service");
+const AuthMessages = require("./auth.messages");
 class AuthController {
-    #service;
-    constructor() {
-        this.#service = authService;
-        autoBind(this);
-    }
+  #service;
+  constructor() {
+    this.#service = authService;
+    autoBind(this);
+  }
 
-    async sendOtp(req, res, next){
-        try {
-            const {mobile} = req.body;
-            const result = await this.#service.sendOtp(mobile);
-            return res.status(200).json({
-                status: 200,
-                success: true,
-                message: AuthMessages.sendOtpSuccessfully,
-                data: result
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
+  async sendOtp(req, res, next) {
+    try {
+      const { mobile } = req.body;
+      const result = await this.#service.sendOtp(mobile);
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        message: AuthMessages.sendOtpSuccessfully,
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }   
+  }
 }
-module.exports = new AuthController(); 
+module.exports = new AuthController();
